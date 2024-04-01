93226efb74d4e5e28a70914aad65df2b1554fa49
william-nyman <32524070+williamnyman@users.noreply.github.com>
2024-03-31 17:57:39 -0700

    Update ddintegrated.py

--- ddintegrated.py
+++ ddintegrated.py
@@ -3,15 +3,15 @@
 import sqlite3
 
 # connect to database
-connection = sqlite3.connect("courses.db")
+connection = sqlite3.connect("test")
 
 # open cursor to perform database operations
 cursor = connection.cursor()
 
 # populate the database
-### right here i want to include something like a text file with all 
-#   the create table and insert statements for the database that way 
-#   i dont need like a billion lines of code                        ###
+# right here i want to include something like a text file with all 
+# the create table and insert statements for the database that way 
+# i dont need like a billion lines of code                        
 
 # form the query
 query = ## this will change once I have Schema figured out ##
